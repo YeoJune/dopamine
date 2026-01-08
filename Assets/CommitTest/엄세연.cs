f1a46a92ee307b46593ba6418cc84e31ee1e9b21
seyeon725 <seyeon0725@naver.com>
2026-01-09 01:27:14 +0900

    엄세연 커밋 테스트

---
+++ Assets/CommitTest/엄세연.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 안녕하세요
+        // 25 컴퓨터학과
+        // 엄세연입니다.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
